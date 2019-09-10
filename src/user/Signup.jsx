1a5607e1cdf4e78be80c2f130edfd4b0e36ffa45
rebSen <archivesreb@gmail.com>
2019-09-10 12:29:50 +0200

    env variable

--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => (
   <Layout title="Signup" description="Signup please : )">
-    3
+    3 {API}
   </Layout>
 );
 
