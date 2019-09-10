a1fc7145010ec355069ad7ac05cddb7ad8b34bae
rebSen <archivesreb@gmail.com>
2019-09-10 11:53:37 +0200

    styling Menu IsActive const

--- src/core/Menu.jsx
+++ src/core/Menu.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
-const Menu = () => (
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
+
+const Menu = ({ history }) => (
   <div>
     <ul className="nav nav-tabs bg-primary">
       <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link className="nav-link" to="/" style={isActive(history, "/")}>
           Home
         </Link>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/signin">
+        <Link
+          className="nav-link"
+          to="/signin"
+          style={isActive(history, "/signin")}
+        >
           Signin
         </Link>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/signup">
+        <Link
+          className="nav-link"
+          to="/signup"
+          style={isActive(history, "/signup")}
+        >
           Signup
         </Link>
       </li>
