9b9bf822dac356419723953d147a3a07470ff74a
rebSen <archivesreb@gmail.com>
2019-09-16 14:15:48 +0200

    remove error of double js extension 2/2 on isAdmin

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
-import { createCategory } from "./apiAdmin.js";
+import { createCategory } from "./apiAdmin";
 
 const AddCategory = () => {
   const [name, setName] = useState("");
