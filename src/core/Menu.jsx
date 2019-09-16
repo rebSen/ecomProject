fe6297f4b626ee2f4fd08fdea5b288cc7047a652
rebSen <archivesreb@gmail.com>
2019-09-16 11:30:14 +0200

    menu item title and liked component depending on admin or user role
    conditions

--- src/core/Menu.jsx
+++ src/core/Menu.jsx
@@ -19,15 +19,29 @@ const Menu = ({ history }) => (
         </Link>
       </li>
 
-      <li className="nav-item">
-        <Link
-          className="nav-link"
-          style={isActive(history, "/user/dashboard")}
-          to="/user/dashboard"
-        >
-          Dashboard
-        </Link>
-      </li>
+      {isAuthenticated() && isAuthenticated().user.role === 0 && (
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/user/dashboard")}
+            to="/user/dashboard"
+          >
+            User Dashboard
+          </Link>
+        </li>
+      )}
+
+      {isAuthenticated() && isAuthenticated().user.role === 1 && (
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/admin/dashboard")}
+            to="/admin/dashboard"
+          >
+            Admin Dashboard
+          </Link>
+        </li>
+      )}
 
       {!isAuthenticated() && (
         <Fragment>
@@ -52,7 +66,6 @@ const Menu = ({ history }) => (
           </li>
         </Fragment>
       )}
-
       {isAuthenticated() && (
         <li className="nav-item">
           <span
