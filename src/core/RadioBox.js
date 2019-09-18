a0dacc1798673757679f5cc48004670df374d9d8
rebSen <archivesreb@gmail.com>
2019-09-18 12:03:32 +0200

    display radioBox with datas from prices
    map on prices from fixedprices.js

--- src/core/RadioBox.js
+++ src/core/RadioBox.js
@@ -2,13 +2,31 @@ import React, { useState, useEffect, Fragment } from "react";
 
 const RadioBox = ({ prices }) => {
   const [value, setValue] = useState(0);
-  return (
-    <Fragment>
-      {/* {JSON.stringify(prices)} */}
-      <input type="radio" className="mr-2 ml-4" />
-      <label className="form-check-label">Name</label>
-    </Fragment>
-  );
+
+  const handleChange = () => {
+    //
+  };
+
+  return prices.map((p, i) => (
+    <div key={i} className="list-unstyled">
+      <input
+        onChange={handleChange}
+        type="Radio"
+        value={`${p._id}`}
+        className="mr-2 ml-4"
+      />
+      <label className="form-check-label">{p.name}</label>
+    </div>
+  ));
+
+  // (
+  //   <Fragment>
+  //     {/* {JSON.stringify(prices)} */}
+  //     <input type="radio" className="mr-2 ml-4" />
+  //     <label className="form-check-label">Name</label>
+  //     {}
+  //   </Fragment>
+  // );
 };
 
 export default RadioBox;
