4d2849b2a0c1774a4c57b510827875e53c9945b8
rebSen <archivesreb@gmail.com>
2019-09-19 22:19:07 +0200

    grabbing category and search by consoleLog in Search component

--- src/core/Search.js
+++ src/core/Search.js
@@ -26,9 +26,49 @@ const Search = () => {
   useEffect(() => {
     loadCatgories();
   }, []);
+  const searchData = () => {
+    console.log(search, category);
+  };
+  const searchSubmit = e => {
+    e.preventDefault();
+    searchData();
+  };
+
+  const handleChange = name => event => {
+    setData({ ...data, [name]: event.target.value, searched: false });
+  };
+
+  const searchForm = () => (
+    <form onSubmit={e => searchSubmit(e)}>
+      <span className="input-group-text">
+        <div className="input-group input-group-lg">
+          <div className="input-group-prepend">
+            <select className="btn mr-2" onChange={handleChange("category")}>
+              <option value="all">Pick Category</option>
+              {categories.map((cat, i) => (
+                <option key={i} value={cat._id}>
+                  {cat.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <input
+            type="search"
+            className="form-control"
+            onChange={handleChange("search")}
+            placeholder="search by name"
+          />
+        </div>
+        <div className="btn input-group-append" style={{ border: "none" }}>
+          <button className="input-group-text">Search</button>
+        </div>
+      </span>
+    </form>
+  );
+
   return (
-    <div>
-      <h2> Search bar {JSON.stringify(categories)}</h2>
+    <div className="row">
+      <div className="container mb-3"> Search bar {searchForm()}</div>
     </div>
   );
 };
