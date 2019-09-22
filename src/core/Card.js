35c97315ca3d96652c86262019c2ca390eb4aed9
rebSen <archivesreb@gmail.com>
2019-09-22 18:49:12 +0200

    Display Stock AddDate on card ok BUT problem to display cat Name
    instead of name display category id

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShowImage from "./ShowImage";
+import moment from "moment";
 
 const Card = ({ product, showViewProductButton = true }) => {
-  //console.log(product);
+  console.log(product.category);
   const showViewButton = showViewProductButton => {
     return (
       showViewProductButton && (
@@ -16,17 +17,42 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
+  const showAddToCardButton = () => {
+    return (
+      <button className="btn btn-outline-warning mt-2 mb-2 mr-2">
+        Add to Card
+      </button>
+    );
+  };
+
+  const showStock = quantity => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">{`${quantity} dvd yet in Stock`}</span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of Stock</span>
+    );
+  };
+
   return (
     <div className="card">
-      <div className="card-header">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
-        <p>{product.description.substring(0, 100)}</p>
-        <p>${product.price}</p>
+        <p className="lead mt-2">
+          <p>{product.description.substring(0, 100)}</p>
+        </p>
+
+        <p className="black-10">${product.price}</p>
+        <p className="black-9">
+          Category : {product.category && product.category}
+        </p>
+        <p className="black-8">
+          Added on {moment(product.createdAt).fromNow()}
+        </p>
+        {showStock(product.quantity)}
+        <br />
         {showViewButton(showViewProductButton)}
-        <button className="btn btn-outline-warning mt-2 mb-2 mr-2">
-          Add to card
-        </button>
+        {showAddToCardButton()}
       </div>
     </div>
   );
