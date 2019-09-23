4afd53c9d6bd043f7dc22c8ed58107885701abd6
rebSen <archivesreb@gmail.com>
2019-09-23 20:44:03 +0200

    Cart system
    Cart component - link in menu - route - style of item.length in menu

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import moment from "moment";
+import { addItem } from "./cartHelpers";
 
 const Card = ({ product, showViewProductButton = true }) => {
+  const [redirect, setRedirect] = useState(false);
   console.log(product.category);
+
   const showViewButton = showViewProductButton => {
     return (
       showViewProductButton && (
@@ -19,12 +22,26 @@ const Card = ({ product, showViewProductButton = true }) => {
 
   const showAddToCardButton = () => {
     return (
-      <button className="btn btn-outline-warning mt-2 mb-2 mr-2">
+      <button
+        onClick={addToCart}
+        className="btn btn-outline-warning mt-2 mb-2 mr-2"
+      >
         Add to Card
       </button>
     );
   };
 
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = redirect => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
   const showStock = quantity => {
     return quantity > 0 ? (
       <span className="badge badge-primary badge-pill">{`${quantity} dvd yet in Stock`}</span>
@@ -37,6 +54,7 @@ const Card = ({ product, showViewProductButton = true }) => {
     <div className="card">
       <div className="card-header name">{product.name}</div>
       <div className="card-body">
+        {shouldRedirect(redirect)}
         <ShowImage item={product} url="product" />
         <p className="lead mt-2">
           <p>{product.description.substring(0, 100)}</p>
---
+++ src/core/Cart.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, Fragment } from "react";
+import Layout from "./Layout";
+import { Link } from "react-router-dom";
+import { getCart } from "./cartHelpers";
+import Card from "./Card";
+
+const Cart = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setItems(getCart);
+  }, []);
+
+  const showItems = items => {
+    return (
+      <div>
+        <h2> Your cart has {`${items.length}`} items</h2>
+        <hr />
+        {items.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+    );
+  };
+  const noItemsMessage = () => (
+    <Fragment>
+      <h2> Your cart is empty</h2>
+      <br />
+      <Link to="/shop">Continue shopping</Link>{" "}
+    </Fragment>
+  );
+
+  return (
+    <Layout
+      title="Shopping Cart"
+      description="Manage Your card items."
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-6">
+          {items.length > 0 ? showItems(items) : noItemsMessage()}
+        </div>
+        <div className="col-6">
+          Show checkout options / shipping adress / etc...
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Cart;
