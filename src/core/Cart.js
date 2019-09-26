147fc0d29406232fd4d1c67f04b55c045df38901
rebSen <archivesreb@gmail.com>
2019-09-26 13:15:14 +0200

    tweak Correct a map key and a function call

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -7,7 +7,7 @@ import Card from "./Card";
 const Cart = () => {
   const [items, setItems] = useState([]);
   useEffect(() => {
-    setItems(getCart);
+    setItems(getCart());
   }, []);
 
   const showItems = items => {
