5f0e2604d0660113700c05bd51e2a7b744e76f9a
rebSen <archivesreb@gmail.com>
2019-10-02 18:16:45 +0200

    Fetch braintreeClientToken in Ckeckout component

--- src/core/CheckOut.js
+++ src/core/CheckOut.js
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getProducts } from "./apiCore";
+import { getProducts, getBraintreeClientToken } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 
 const CheckOut = ({ products }) => {
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {},
+    address: ""
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    console.log("YO1", userId, token);
+    getBraintreeClientToken(userId, token).then(data => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({ ...data, clientToken: data.clientToken });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -78,3 +78,19 @@ export const listRelated = productId => {
     })
     .catch(err => console.log(err));
 };
+
+export const getBraintreeClientToken = (userId, token) => {
+  return fetch(`${API}/braintree/getToken/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(res => {
+      console.log("RES", res);
+      return res.json();
+    })
+    .catch(err => console.log("ERROR??", err));
+};
