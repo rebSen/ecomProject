824ca8afd983aa4b0de78c3b600901f8a781492d
rebSen <archivesreb@gmail.com>
2019-10-06 21:54:00 +0200

    show detail of orders in Orders component

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -31,6 +31,15 @@ const Orders = () => {
     }
   };
 
+  const showInput = (key, value) => (
+    <div className="input-group mb-2 mr-sm-2">
+      <div className="input-group-prepend">
+        <div className="input-group-text">{key}</div>
+        <input type="text" value={value} className="form-control" readOnly />
+      </div>
+    </div>
+  );
+
   return (
     <Layout
       title="Add a new category"
@@ -56,7 +65,7 @@ const Orders = () => {
                     Transaction ID : {o.transaction_id}
                   </li>
                   <li className="list-group-item"> Amount : ${o.amount}</li>
-                  <li className="list-group-item"> Ordered by : {o.user}</li>
+                  <li className="list-group-item"> USER (probl) : {o.user}</li>
                   <li className="list-group-item">
                     Ordered on : {moment(o.createdAt).fromNow()}
                   </li>
@@ -67,6 +76,18 @@ const Orders = () => {
                 <h3 className="mt-4 mb-4 font-italic">
                   Total Product in the order: {o.products.length}
                 </h3>
+                {o.products.map((p, pIndex) => (
+                  <div
+                    className="mb-4"
+                    key={pIndex}
+                    style={{ padding: "20px", border: "1px solid indigo" }}
+                  >
+                    {showInput("Product name", p.name)}
+                    {showInput("Product price", p.price)}
+                    {showInput("Product total", p.count)}
+                    {showInput("Product Id", p._id)}
+                  </div>
+                ))}
               </div>
             );
           })}
