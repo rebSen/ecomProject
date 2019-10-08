55be235d774d7c27f114fd60a6bee55ddc4288e1
rebSen <archivesreb@gmail.com>
2019-10-08 17:48:24 +0200

    route to ManageProduct

--- src/Routes.js
+++ src/Routes.js
@@ -16,6 +16,7 @@ import Product from "./core/Product";
 import Cart from "./core/Cart";
 import Orders from "./admin/Orders";
 import Profile from "./user/Profile";
+import ManageProducts from "./admin/ManageProducts";
 
 const Routes = () => {
   return (
@@ -33,6 +34,7 @@ const Routes = () => {
         <Route path="/cart" exact component={Cart} />
         <AdminRoute path="/admin/orders" exact component={Orders} />
         <PrivateRoute path="/profile/:userId" exact component={Profile} />
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
       </Switch>
     </BrowserRouter>
   );
