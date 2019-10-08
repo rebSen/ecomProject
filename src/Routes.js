fe7c0917cdfe99268fc8dc5aab591842badd5313
rebSen <archivesreb@gmail.com>
2019-10-08 23:38:37 +0200

    route to UpdateProduct component

--- src/Routes.js
+++ src/Routes.js
@@ -17,6 +17,7 @@ import Cart from "./core/Cart";
 import Orders from "./admin/Orders";
 import Profile from "./user/Profile";
 import ManageProducts from "./admin/ManageProducts";
+import UpdateProducts from "./admin/UpdateProducts";
 
 const Routes = () => {
   return (
@@ -35,6 +36,11 @@ const Routes = () => {
         <AdminRoute path="/admin/orders" exact component={Orders} />
         <PrivateRoute path="/profile/:userId" exact component={Profile} />
         <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProducts}
+        />
       </Switch>
     </BrowserRouter>
   );
