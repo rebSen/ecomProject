15fb94d7ed0d4b77dbdeb8cab9253638e516e4b6
rebSen <archivesreb@gmail.com>
2019-10-08 18:18:21 +0200

    delete product ok and link to update product ok
    BUT problem with getProducts that only get 6 product

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -1,16 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
+import { getProducts, deleteProduct } from "./apiAdmin";
 import { Link } from "react-router-dom";
 
 const ManageProducts = () => {
+  const [products, setProducts] = useState([]);
+  const { user, token } = isAuthenticated();
+
+  const loadProducts = () => {
+    getProducts().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+        setProducts(data);
+      }
+    });
+  };
+
+  const destroy = productId => {
+    deleteProduct(productId, user._id, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        loadProducts();
+      }
+    });
+  };
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
   return (
     <Layout
       title="Manage Product"
       description="Perform CRUD on products"
       className="container-fluid"
     >
-      manage
+      <div className="row">
+        <div className="col-12">
+          <h2 className="text-center">Total {products.length} products</h2>
+          <hr />
+          <ul className="list-group">
+            {products.map((p, i) => (
+              <li
+                key={i}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <strong>{p.name}</strong>
+
+                <Link to={`/admin/product/update/${p._id}`}>
+                  <span className="badge badge-warning badge-pill">Update</span>
+                </Link>
+
+                <span
+                  onClick={() => destroy(p._id)}
+                  className="badge badge-danger badge-pill"
+                >
+                  Delete
+                </span>
+              </li>
+            ))}
+          </ul>
+          <br />
+        </div>
+      </div>
     </Layout>
   );
 };
