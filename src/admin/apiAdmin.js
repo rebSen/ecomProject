63bef42273c75969a6376d244de9cd76ab9a5686
rebSen <archivesreb@gmail.com>
2019-10-08 17:49:39 +0200

    product crud requests in apiAdmin

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -90,3 +90,60 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
     })
     .catch(err => console.log(err));
 };
+
+// get all products
+// get a single products
+// update a single products
+// delete a single products
+
+export const getProducts = () => {
+  return fetch(`${API}/products`, {
+    method: "GET"
+  })
+    .then(res => {
+      return res.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const deleteProduct = (productId, userId, token) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(res => {
+      console.log("STATUS", res);
+      return res.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const getProduct = productId => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET"
+  })
+    .then(res => {
+      return res.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product // no stringify because of the picture
+  })
+    .then(res => {
+      console.log("STATUS", res);
+      return res.json();
+    })
+    .catch(err => console.log(err));
+};
