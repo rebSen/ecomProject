2be6b0cb538c3ffd3a840340e99919cd9cea27e7
rebSen <archivesreb@gmail.com>
2019-11-08 13:00:07 +0100

    init title component

---
+++ src/core/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = () => (
+  // <div className="title row">
+  <div className="title">Réel Factory & La Famille Digitale [2005 - 2017] </div>
+  // </div>
+);
+
+export default Title;
